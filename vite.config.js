cfa8b61f88066276237d77c358683e59d7fca911
michaellefebvre416 <michaellefebvre416@gmail.com>
2024-11-29 12:38:29 +0000

    begin

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    hmr: {
+      host: '9000-idx-award-winning-websitegit-1732761285886.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev',
+      protocol: 'wss', // Try 'wss' if using HTTPS, otherwise, 'ws'
+      clientPort: 443,
+    }
+  },
   plugins: [react()],
 })
